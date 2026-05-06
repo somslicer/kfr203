456ff912a6bd7ec9dcec3227ff8eab3c93a5514c
somslicer <somashekar@slicerpl.com>
2026-05-06 18:04:23 +0530

    Replace created_datetime with invc_post_date in oracle_queries; add oracle_files_updated

--- oracle_files_updated/Store Sales Summary.sql
+++ oracle_files_updated/Store Sales Summary.sql
@@ -1,29 +1,25 @@
 select r.sbs_name as "SBS NAME",
-       r.dcs_code as "DCS CODE",
-       r.vend_code as "VENDOR CODE",
-       r.description1 as "DESCRIPTION1",
+       r.store_code as "STORE CODE",
        r.QtySold as "SOLD QTY",
-	   r.QtyRet as "RETURN QTY",
-	   r.EXTOP as "GROSS SALES",
+       r.QtyRet as "RETURN QTY",
+   r.EXTOP as "GROSS SALES",
        r.EXTOPT as "GROSS SALES W TAX",
        r.EXTD as "DISC AMT",
-       round(case when r.EXTOP = 0 then 0 else (r.EXTD/decode(r.use_vat,1,r.EXTOPT,r.EXTOP)*100) end, 2) as "DISC%",
-       r.EXTP as "NET SALES",
-	   r.EXTPT as "NET SALES W TAX",
+round(case when r.EXTOP = 0 then 0 else (r.EXTD/decode(r.use_vat,1,r.EXTOPT,r.EXTOP)*100) end, 2) as "DISC%",
+	   r.EXTP as "NET SALES",
+       r.EXTPT as "NET SALES W TAX",
        r.EXTC as "TOTAL COST",
        round(r.EXTP - r.EXTC, 2) as "MARGIN AMT",
        round((case when r.EXTP = 0 then 0 else (r.EXTP - r.EXTC)/r.EXTP * 100 end),2) as "MARGIN%"
 from (
     select s.sbs_name,
-           d.dcs_code,
-           v.vend_code,
-           i.description1,
-a.use_vat, 
+           a.store_code,
+           a.store_name,
+a.use_vat,
            sum((case when b.item_type = 2 then 0 else b.qty end)) as QtySold,
-		   sum((case when b.item_type = 2 then b.qty*-1 else 0 end)) as QtyRet,
+	 sum((case when b.item_type = 2 then b.qty*-1 else 0 end)) as QtyRet,
 		   round(sum((case when b.item_type = 2 then 0 else b.qty end) * decode(a.use_vat,1,b.orig_price-b.orig_tax_amt, b.orig_price)),2) as EXTOP,
            round(sum((case when b.item_type = 2 then 0 else b.qty end) * decode(a.use_vat,1,b.orig_price, b.orig_price+b.orig_tax_amt)),2) as EXTOPT,
-		   round(sum((case when b.item_type = 2 then b.qty*-1 else 0 end) * decode(a.use_vat,1,b.orig_price, b.orig_price+b.orig_tax_amt)),2) as EXTRPT,
 		   round(sum((case when b.item_type = 2 then b.qty*-1 else b.qty end) * decode(a.use_vat,1,b.price-b.tax_amt-b.tax2_amt, b.price)),2) as EXTP,
 		   round(sum((case when b.item_type = 2 then b.qty*-1 else b.qty end) * decode(a.use_vat,1,b.price, b.price+b.tax_amt+b.tax2_amt)),2) as EXTPT,
            round(sum((case when b.item_type = 2 then b.qty*-1 else b.qty end) * (b.disc_amt)), 2) as EXTD,
@@ -39,5 +35,5 @@ a.use_vat,
        and =-=a.store_no=-=
        and =-=a.store_code=-=
        and =-=trunc(a.invc_post_date)=-=
-    group by s.sbs_name, d.dcs_code, v.vend_code, i.description1, a.use_vat
+    group by s.sbs_name, a.store_code, a.store_name, a.use_vat
 ) r
--- oracle_queries/On Hand-Sales-RCVD-Trans-ADJ.sql
+++ oracle_queries/On Hand-Sales-RCVD-Trans-ADJ.sql
@@ -37,28 +37,28 @@ JOIN (
     SUM(CASE WHEN d1.doc_close_rank = 1 THEN d1.close_Qty ELSE 0 END) AS Closing_ONHAND
   FROM (
   SELECT
-      d2.sbs_no, d2.store_no, d2.doc_type, d2.created_datetime,
+      d2.sbs_no, d2.store_no, d2.doc_type, d2.invc_post_date,
       d2.item_sid, d2.open_Qty, d2.qty, d2.close_Qty,
-      rank() OVER (PARTITION BY d2.sbs_no, d2.store_no, d2.item_sid ORDER BY d2.created_datetime) AS doc_open_rank,
-      rank() OVER (PARTITION BY d2.sbs_no, d2.store_no, d2.item_sid ORDER BY d2.created_datetime DESC) AS doc_close_rank
+      rank() OVER (PARTITION BY d2.sbs_no, d2.store_no, d2.item_sid ORDER BY d2.invc_post_date) AS doc_open_rank,
+      rank() OVER (PARTITION BY d2.sbs_no, d2.store_no, d2.item_sid ORDER BY d2.invc_post_date DESC) AS doc_close_rank
   FROM(
     SELECT 
       d.sbs_no,
       d.store_no,
       d.doc_type,
-      d.created_datetime,
+      d.invc_post_date,
       d.item_sid,
-      SUM (d.qty) OVER (PARTITION BY d.sbs_no, d.store_no, d.item_sid ORDER BY d.created_datetime DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS open_Qty,
+      SUM (d.qty) OVER (PARTITION BY d.sbs_no, d.store_no, d.item_sid ORDER BY d.invc_post_date DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS open_Qty,
       d.qty,
       CASE WHEN DOC_TYPE ='INVN' THEN d.qty
-          ELSE (SUM (d.qty) OVER (PARTITION BY d.sbs_no, d.store_no, d.item_sid ORDER BY d.created_datetime DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW))  - d.qty 
+          ELSE (SUM (d.qty) OVER (PARTITION BY d.sbs_no, d.store_no, d.item_sid ORDER BY d.invc_post_date DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW))  - d.qty 
           END AS close_Qty
     FROM (
       SELECT 
           s.sbs_no,
           st.store_no,
           'INVN' AS doc_type,
-          (SYSTIMESTAMP) AS created_datetime,
+          (SYSTIMESTAMP) AS invc_post_date,
           i.sid AS item_sid,
           NVL(iq.qty, 0) AS qty
       FROM rps.invn_sbs_item i
@@ -72,7 +72,7 @@ JOIN (
           a.sbs_no,
           a.store_no,
           'SALE' AS doc_type,
-          (a.created_datetime) AS created_datetime,
+          (a.invc_post_date) AS invc_post_date,
           b.invn_sbs_item_sid AS item_sid,
           SUM(CASE WHEN b.item_type = 2 THEN b.qty * -1  ELSE b.qty END) qty
         FROM rps.document a
@@ -87,13 +87,13 @@ JOIN (
           and =-=s.sbs_no=-= and =-=st.store_no=-=
       GROUP BY a.sbs_no,
           a.store_no,
-          (a.created_datetime),
+          (a.invc_post_date),
           b.invn_sbs_item_sid
       UNION ALL   
       SELECT s.sbs_no,
           st.store_no,
           'VOUCHER' AS doc_type,
-          (a.created_datetime) AS created_datetime,
+          (a.invc_post_date) AS invc_post_date,
           b.item_sid,
           SUM(CASE WHEN a.vou_type = 1 THEN b.qty  ELSE b.qty *-1 END) qty
         FROM rps.voucher a
@@ -106,13 +106,13 @@ JOIN (
           and =-=s.sbs_no=-= and =-=st.store_no=-=
         GROUP BY s.sbs_no,
           st.store_no,
-          (a.created_datetime),
+          (a.invc_post_date),
           b.item_sid    
       UNION ALL
       SELECT s.sbs_no,
           st.store_no,
           'SLIP' AS doc_type,
-          (a.created_datetime) AS created_datetime,
+          (a.invc_post_date) AS invc_post_date,
           b.item_sid,
           SUM(b.qty) qty
         FROM rps.SLIP a
@@ -123,13 +123,13 @@ JOIN (
           and =-=s.sbs_no=-= and =-=st.store_no=-=
         GROUP BY s.sbs_no,
           st.store_no,
-          (a.created_datetime),
+          (a.invc_post_date),
           b.item_sid
       UNION ALL  
       SELECT s.sbs_no,
           st.store_no,
           'ADJUSTMENT' AS doc_type,
-          (a.created_datetime) AS created_datetime,
+          (a.invc_post_date) AS invc_post_date,
           b.item_sid,
           SUM(NVL(adj_value,0)- NVL(orig_value,0)) * -1 AS qty
         FROM rps.Adjustment a
@@ -142,12 +142,12 @@ JOIN (
           and =-=s.sbs_no=-= and =-=st.store_no=-=
         GROUP BY s.sbs_no,
           st.store_no,
-          (a.created_datetime),
+          (a.invc_post_date),
           b.item_sid
            ) d
     )d2
      WHERE 1 = 1
-  AND =-=TRUNC(d2.created_datetime)=-=
+  AND =-=TRUNC(d2.invc_post_date)=-=
   ) d1
   GROUP BY   d1.sbs_no,
     d1.store_no,
@@ -163,7 +163,7 @@ JOIN (
       AND d2.vou_class=2 AND d2.slip_flag=1 AND d2.held=0 AND d2.verified=0 AND d2.active=1
       AND =-=s.sbs_no=-= 
       AND =-=st.store_no=-=
-      AND =-=TRUNC(d2.created_datetime)=-= 
+      AND =-=TRUNC(d2.invc_post_date)=-= 
       GROUP BY s.sbs_no,st.store_no,b.item_sid) intran
   ON s.sbs_no = intran.sbs_no AND st.store_no = intran.store_no AND iq.invn_sbs_item_sid = intran.item_sid
 WHERE 1 = 1
